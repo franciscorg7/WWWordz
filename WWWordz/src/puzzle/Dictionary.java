6f7291e943a73e0d03f06e578c59ef65ab45022d
franciscorg7 <kikofranciscorg@gmail.com>
2020-04-08 22:38:43 +0100

    dictionary imported
    dictionary parsed into trie
    
    trie implemented

--- WWWordz/src/puzzle/Dictionary.java
+++ WWWordz/src/puzzle/Dictionary.java
@@ -1,14 +1,29 @@
 package puzzle;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.Locale;
+
+import puzzle.Trie.Node;
 import puzzle.Trie.Search;
 
 public class Dictionary
 extends java.lang.Object {
 	
+	private static Dictionary single_instance = null;
 	
-	
+	// Makes sure it only creates one and only instance of Dictionary
 	public static Dictionary getInstance() {
-		return null;
+		
+		if(single_instance == null) 
+            single_instance = new Dictionary(); 
+  
+        return single_instance;
 	}
 	
 	public String getRandomLargeWord() {
@@ -18,5 +33,63 @@ extends java.lang.Object {
 	
 	public Search startSearch() {
 		return null;
+	}
+	
+	public static Trie parseDictionary() throws IOException {
+		
+		Trie trie = new Trie();
+		
+		BufferedReader myBuffer = new BufferedReader(
+				new InputStreamReader(new FileInputStream("src/puzzle/pt-PT-AO.dic"), "UTF-8"));
+
+		String line = myBuffer.readLine();
+
+		while(line != null) {
+			String word = "";
+			for(int i = 0; i < line.length(); i++) {
+				if(!Character.isLetter(line.charAt(i)))
+					break;
+				word += line.charAt(i);
+			}
+
+			word = Normalizer.normalize(word.toUpperCase(Locale.ENGLISH), Form.NFD)
+					.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+			
+			trie.put(word);
+			
+			line = myBuffer.readLine();
+		}
+
+		myBuffer.close();
+		
+		return trie;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+        BufferedReader myBuffer = new BufferedReader(new InputStreamReader(
+          new FileInputStream("src/puzzle/pt-PT-AO.dic"), "UTF-8"));
+         
+        String line = myBuffer.readLine();
+        
+        while (line != null) {
+        	String word = "";
+        	for(int i = 0; i < line.length(); i++) {
+        		if(!Character.isLetter(line.charAt(i))) break;
+        		word += line.charAt(i); 
+        	}
+        	
+        	word = Normalizer.normalize(word.toUpperCase(Locale.ENGLISH),Form.NFD).
+            replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+            System.out.println(word);
+            line = myBuffer.readLine();
+        }
+ 
+        myBuffer.close();
+        
+        Trie t = parseDictionary();
+        Node n = t.searchNode("CRIA");
+        System.out.println("Word: " + n.isWord);
+        System.out.println("Leaf: " + n.isLeaf());
 	}
 }
--- WWWordz/src/puzzle/Trie.java
+++ WWWordz/src/puzzle/Trie.java
@@ -1,14 +1,18 @@
 package puzzle;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Trie 
 extends java.lang.Object
 implements java.lang.Iterable<String>{
 
+	private Node root;
+	
 	public Trie() {
-		
+		root = new Node();
 	}
 	
 	public String getRandomLargeWord() {
@@ -16,12 +20,47 @@ implements java.lang.Iterable<String>{
 	}
 	
 	public void put(String word) {
-		
+		HashMap<Character, Node> children = root.children;
+		 
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+ 
+            Node t;
+            if(children.containsKey(c)) {
+            	t = children.get(c);
+            } else {
+                t = new Node(c);
+                children.put(c, t);
+            }
+ 
+            children = t.children;
+ 
+            // Set node as word terminator
+            if(i == word.length()-1)
+                t.isWord = true;  
+        }
 	}
 	
 	public Search startSearch() {
 		return null;
 	}
+	
+	public Node searchNode(String str){
+        Map<Character, Node> children = root.children; 
+       
+        Node t = null;
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if(children.containsKey(c)){
+                t = children.get(c);
+                children = t.children;
+            } else{
+            	return null;
+            }
+        }
+ 
+        return t;
+    }
 	
 	@Override
 	public Iterator<String> iterator() {
@@ -30,43 +69,66 @@ implements java.lang.Iterable<String>{
 	}
 	
 	public class Node{
-		int depth;
-		String value;
-		List<Node> children;
+		char val;
+		HashMap<Character, Node> children = new HashMap<Character, Node>();
 		Node parent;
+		boolean isWord;
 		
 		public Node() {
 		}
 		
-		public Node(int depth, String val, List<Node> children, Node parent) {
-			this.depth = depth;
-			this.value = val;
-			this.children = children;
-			this.parent = parent;
+		public Node(char c) {
+			this.val = c;
+		}
+		
+		// Check if node is a leaf
+		public boolean isLeaf() {
+			if(this.children.isEmpty()) return true;
+			return false;
 		}
 	}
 	
-	public class NodeIterator{
+	public class NodeIterator
+	extends java.lang.Object
+	implements java.util.Iterator<java.lang.String>, java.lang.Runnable{
+		private String nextWord;
+		private boolean terminated;
+		private Thread thread;
 		
+		public NodeIterator() {
+		}
+		
+		public boolean hasNext() {
+			return this.terminated;
+		}
+		
+		public String next() {
+			return null;
+		}
+		
+		public void run() {
+		}
 	}
 	
-	public class Search	{
+	public static class Search	
+	extends java.lang.Object {
 		Node node;
 		
 		public Search(Node node) {
-			
+			this.node = node;
 		}
 		
 		public Search(Search search) {
-			
+			this.node = search.node;
 		}
 		
 		private boolean continueWith(char letter) {
+			if(this.node.children != null) return true;
 			return false;
 		}
 		
 		private boolean isWord() {
-			return false;
+			return this.node.isWord; 
 		}
 		
 	}
